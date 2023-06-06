07f86a13f2a2642408c670e595fd7af2479fd152
Saad <saadshaikh203@gmail.com>
2023-06-06 15:03:21 +0530

    Aritrhamtic_calci_cahnges

--- arithmatic-calci.py
+++ arithmatic-calci.py
@@ -5,3 +5,5 @@ print('Subtraction of',x,'and',y,'is',(x-y))
 print('Division of',x,'and',y,'is', int((x/y)))
 print('Remainder after performing division of',x,'and',y,'is',(x%y))
 print('Multiplication of',x,'and',y,'is',(x*y))
+
+# hi my name is saad 1 to the 3 to 9 biatch.
